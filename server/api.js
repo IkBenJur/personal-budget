3385fc571a8b1ece0cd43c57382f6a2fc726b327
IkBenJur <jurspammer@gmail.com>
2024-12-13 23:03:28 +0100

    Created envelopes router

--- server/api.js
+++ server/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const envelopesRouter = require("./routers/envelopes");
 const apiRouter = express.Router();
 
 apiRouter.get("/", (req, res) => {
-    res.send("Hello, World!")
-})
+  res.send("Hello, World!");
+});
+
+apiRouter.use("/envelopes", envelopesRouter);
 
 module.exports = apiRouter;
